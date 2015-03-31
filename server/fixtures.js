c16d0a90f8826d9594b1c9fe7f091e9177f16649
UlyXB <thyle@thyle.com>
2015-03-31 20:20:53 +0100

    Added basic voting algorithm

--- server/fixtures.js
+++ server/fixtures.js
@@ -19,7 +19,9 @@ if (Posts.find().count() === 0) {
         author: sacha.profile.name,
         url: 'http://sachagreif.com/introducing-telescope/',
         submitted: new Date(now - 7 * 3600 * 1000),
-        commentsCount: 2
+        commentsCount: 2,
+        upvoters: [],
+        votes: 0
     });
 
     Comments.insert({
@@ -43,7 +45,10 @@ if (Posts.find().count() === 0) {
         userId: thyle._id,
         author: thyle.profile.name,
         url: 'http://meteor.com',
-        submitted: new Date(now - 10 * 3600 * 1000)
+        submitted: new Date(now - 10 * 3600 * 1000),
+        commentsCount: 0,
+        upvoters: [],
+        votes: 0
     });
 
     Posts.insert({
@@ -51,7 +56,10 @@ if (Posts.find().count() === 0) {
         userId: thyle._id,
         author: thyle.profile.name,
         url: 'http://themeteorbook.com',
-        submitted: new Date(now - 12 * 3600 * 1000)
+        submitted: new Date(now - 12 * 3600 * 1000),
+        commentsCount: 0,
+        upvoters: [],
+        votes: 0
     });
 
     for (var i = 0; i < 10; i++) {
@@ -61,7 +69,9 @@ if (Posts.find().count() === 0) {
         userId: thyle._id,
         url: 'http://google.com/?q=test-' + i,
         submitted: new Date(now - i * 3600 * 1000),
-        commentsCount: 0
+        commentsCount: 0,
+        upvoters: [],
+        votes: 0
     });
   }
 }
